a08af93d257a7cc78022018334991cf561c40ccb
universato <universato@gmail.com>
2022-08-16 12:49:46 +0900

    Fix style

--- cursor.py
+++ cursor.py
@@ -2,7 +2,7 @@
 from pynput.mouse import Listener
 
 def on_click(x, y, button, pressed):
-    print(format(x, '.2f') + ", " +format(y, '.2f'), button, pressed)
+    print(format(x, '.2f') + ", " + format(y, '.2f'), button, pressed)
 
 with Listener(on_click = on_click) as listener:
     listener.join()
