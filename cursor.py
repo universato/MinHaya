6c6a1e2b794a13e2a2edab08d56940b2d23ac64a
universato <universato@gmail.com>
2022-11-02 03:55:11 +0900

    Refine cursor.py

--- cursor.py
+++ cursor.py
@@ -2,7 +2,8 @@
 from pynput.mouse import Listener
 
 def on_click(x, y, button, pressed):
-    print(format(x, '.2f') + ", " + format(y, '.2f'), button, pressed)
+    if pressed:
+        print('{:.1f}, {:.1f}'.format(x, y))
 
 with Listener(on_click = on_click) as listener:
     listener.join()
