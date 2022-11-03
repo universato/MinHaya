07ebcdaa332ce1c40e79a2f92035e6250973e8bd
universato <universato@gmail.com>
2022-11-04 05:06:14 +0900

    数字を右揃えにして見やすくした

--- cursor.py
+++ cursor.py
@@ -3,7 +3,7 @@ from pynput.mouse import Listener
 
 def on_click(x, y, button, pressed):
     if pressed:
-        print('{:.1f}, {:.1f}'.format(x, y))
+        print('{:5.1f}, {:5.1f}'.format(x, y))
 
 with Listener(on_click = on_click) as listener:
     listener.join()
