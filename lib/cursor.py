73c10ba8aeae11871d58310c8a75b9274b6865b9
universato <universato@gmail.com>
2022-11-27 00:10:05 +0900

    autopep8 for cursor.py

--- lib/cursor.py
+++ lib/cursor.py
@@ -1,9 +1,11 @@
 # python:cursor.py
 from pynput.mouse import Listener
 
+
 def on_click(x, y, button, pressed):
     if pressed:
         print('{:5.1f}, {:5.1f}'.format(x, y))
 
-with Listener(on_click = on_click) as listener:
+
+with Listener(on_click=on_click) as listener:
     listener.join()
